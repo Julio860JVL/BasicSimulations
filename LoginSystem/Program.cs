a976607a00c331cebf088a8ce45fd8e785882e0a
Julio <Julio@JULIOMSI>
2025-04-02 01:21:27 -0400

    Add project files.

---
+++ LoginSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<string> options = new List<string> { "Create Account", "Login", "Go Back", "Exit" };
+Console.WriteLine("Select from the following options:");
+
+byte counter = 0;
+foreach (string option in options)
+{
+    Console.WriteLine($"{++counter} {option}");
+}
+
