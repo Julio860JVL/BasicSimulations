57af51582c7c0e21103d528227e8c59e0cf3864a
Julio Rodriguez <Julio860JVL@outlook.com>
2025-04-02 03:11:19 -0400

    I spent about 2 hours trying to call Main() without having to modify the code to look like the old .NET structure but the final most reliable and most simple solution was to make it look like the old .NET structure.

---
+++ LoginSystem/OptionsToSelect/AccountCreation.cs
@@ -0,0 +1,46 @@
+﻿namespace LoginSystem.OptionsToSelect
+{
+    public class AccountCreation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } // I am aware that this is not secure, but this is a simple example.
+        public string AccountNumber { get; set; }
+
+        public AccountCreation(string firstName, string lastName, string email, string password, string accountNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+            AccountNumber = accountNumber;
+        }
+
+        public static void CreateAccount()
+        {
+            Console.WriteLine("Enter first name:");
+            string firstName = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter last name:");
+            string lastName = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter email:");
+            string email = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine() ?? string.Empty;
+            Console.WriteLine("Enter account number:");
+            string accountNumber = Console.ReadLine() ?? string.Empty;
+
+            AccountCreation account = new AccountCreation(firstName, lastName, email, password, accountNumber);
+            Console.WriteLine("Account created successfully!");
+            Program.Main(new string[] { });
+        }
+
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Account Number: {AccountNumber}");
+        }
+    }
+
+}
--- LoginSystem/Program.cs
+++ LoginSystem/Program.cs
@@ -1,10 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-List<string> options = new List<string> { "Create Account", "Login", "Go Back", "Exit" };
-Console.WriteLine("Select from the following options:");
+using LoginSystem.OptionsToSelect;
 
-byte counter = 0;
-foreach (string option in options)
+internal class Program
 {
-    Console.WriteLine($"{++counter} {option}");
-}
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\nWelcome to the Login System.\n" +
+            "Select from the following options:");
 
+        List<string> options = new List<string> { "Create Account", "Login", "Go Back", "Exit" };
+
+        byte counter = 0;
+        foreach (string option in options)
+        {
+            Console.WriteLine($"{++counter} {option}");
+        }
+
+        string selection = Console.ReadLine();
+        switch (selection)
+        {
+            case "1":
+                AccountCreation.CreateAccount();
+                break;
+            case "2":
+                //Console.WriteLine("Login");
+                break;
+            case "3":
+                //Console.WriteLine("Go Back");
+                break;
+            case "4":
+                //Console.WriteLine("Exit");
+                break;
+            default:
+                Console.WriteLine("Invalid selection. Please try again.");
+                Main(new string[] { });
+                break;
+        }
+    }
+}
